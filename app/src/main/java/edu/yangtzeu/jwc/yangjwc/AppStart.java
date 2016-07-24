f08f87494386b84f1d00094d117b070abc1f765b
yu-pc <18627243224@163.com>
2016-07-24 16:37:01 +0800

    init project

---
+++ app/src/main/java/edu/yangtzeu/jwc/yangjwc/AppStart.java
@@ -0,0 +1,13 @@
+package edu.yangtzeu.jwc.yangjwc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_start);
+    }
+}
